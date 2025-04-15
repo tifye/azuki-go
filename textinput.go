dee1aa7c6e26cc123809418a790941d951c7d86c
Joshua De Matas <dematasjoshua@hotmail.com>
2025-04-15 21:49:34 +0200

    feat: input placeholder and hidden input

--- textinput.go
+++ textinput.go
@@ -4,10 +4,12 @@ import "time"
 
 type TextInputComponent struct {
 	BaseComponent
-	Value      TextSource `json:"initialValue,omitzero"`
-	DebounceMS uint       `json:"debounce,omitzero"`
-	Target     string     `json:"target,omitzero"`
-	Name       string     `json:"name"`
+	Value       TextSource `json:"initialValue,omitzero"`
+	DebounceMS  uint       `json:"debounce,omitzero"`
+	Target      string     `json:"target,omitzero"`
+	Name        string     `json:"name"`
+	Placeholder TextSource `json:"placeholder,omitzero"`
+	HiddenInput bool       `json:"hiddenInput,omitzero"`
 }
 
 func TextInput() TextInputComponent {
@@ -33,5 +35,15 @@ func (ti TextInputComponent) WithDebounce(t time.Duration) TextInputComponent {
 
 func (ti TextInputComponent) WithTarget(t string) TextInputComponent {
 	ti.Target = t
+	return ti
+}
+
+func (ti TextInputComponent) WithPlaceholder(t TextSource) TextInputComponent {
+	ti.Placeholder = t
+	return ti
+}
+
+func (ti TextInputComponent) WithHiddenInput() TextInputComponent {
+	ti.HiddenInput = true
 	return ti
 }
